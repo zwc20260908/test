be38efe343e014209ffb09c9ed58695603b2071e
zwc20260908 <19853223185@163.com>
2026-08-11 16:10:38 +0800

    Add files via upload

--- dynamic_memory_manager.c
+++ dynamic_memory_manager.c
@@ -6,14 +6,14 @@
 功能同意封装所有结构体，基础数据类型，全局链表头*/
 
 
-//统一longlong别名：适配超大内存地址
+//统一longlong别名：适配超大内存地址 
 typedef long long ll;
 
 // 已分配内存块链表结点
 typedef struct AllocNode {
     char id[20];        // 内存块唯一标识
-    ll start;           // 起始地址
-    ll size;            // 占用大小
+    unsigned long start;           // 起始地址
+    unsigned long size;            // 占用大小
     struct AllocNode* next;
 } AllocNode;
 
@@ -35,3 +35,15 @@ typedef struct MoveLog {
 AllocNode* alloc_head = NULL;   // 已分配块链表头
 FreeNode* free_head = NULL;     // 空闲块链表头
 ll total_mem;                   // 模拟内存总大小M
+
+/*模块 2：空闲链表初始化工具函数 init_free
+ 功能：程序启动初始化整块内存为单一空闲块*/
+
+void init_free(ll M) {
+    total_mem = M;
+    // 创建初始空闲块：地址 0，大小 M
+    free_head = (FreeNode*)malloc(sizeof(FreeNode));
+    free_head->start = 0;
+    free_head->size = M;
+    free_head->next = NULL;
+}
